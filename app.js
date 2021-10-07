07e5fbbe83ff795f64d55a0ce6ee24a50bf12fbd
SeifAllah-Ben-Ahmed <benahmed.seifallah@gmail.com>
2021-10-07 16:49:39 +0200

    Setup Basic ExpressJs App

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+app.use(express.json());
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  console.log('middleware');
+  next();
+});
+
+// 2) ROUTE HANDLERS
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+module.exports = app;
---
+++ routes/tourRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const {
+  getAllTours,
+  createTour,
+  deletetour,
+  updateTour,
+  getTour,
+  checkId,
+} = require('../controllers/tourController');
+
+const router = express.Router();
+
+const checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'bad request',
+    });
+  }
+  next();
+};
+
+router.param('id', checkId);
+
+router.route('/').get(getAllTours).post(checkBody, createTour);
+
+router.route('/:id').get(getTour).patch(updateTour).delete(deletetour);
+
+module.exports = router;
