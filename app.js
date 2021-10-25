faad64ab0ec9ff76649e13693ea5bfaa6e844d45
SeifAllah-Ben-Ahmed <benahmed.seifallah@gmail.com>
2021-10-25 16:16:07 +0200

    Security Best Practices

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitier = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -11,7 +17,47 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+const limiter = rateLimit({
+  max: 20,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Limite request
+app.use('/api', limiter);
+
+//Body parser, reading data from body
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
+
+// Data sanitization against NoSQL query  injection
+app.use(mongoSanitier());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'price',
+      'difficulty',
+    ],
+  })
+);
+
+// Data sanitization against XXS
+app.use(xss());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // 2) ROUTE HANDLERS
