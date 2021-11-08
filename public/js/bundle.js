78574b8409bf397359ac70894971dfd40fab528e
SeifAllah-Ben-Ahmed <benahmed.seifallah@gmail.com>
2021-11-09 00:22:34 +0100

    Manage Bookings

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9451,7 +9451,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61436" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57910" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
