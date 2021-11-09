7e25e403454b61620e0c66fabe3898f7b2be59c5
SeifAllah-Ben-Ahmed <benahmed.seifallah@gmail.com>
2021-11-09 22:55:44 +0100

    fix stripe  error

--- controllers/bookingcontroller.js
+++ controllers/bookingcontroller.js
@@ -50,7 +50,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 exports.webhookCheckout = (req, res, next) => {
@@ -58,7 +58,7 @@ exports.webhookCheckout = (req, res, next) => {
   try {
     const signature = req.headers['stripe-signature'];
     event = stripe.webhooks.constructEvent(
-      req.rawBody,
+      req.body,
       signature,
       process.env.STRIPE_SECRET_WEBHOOK
     );
